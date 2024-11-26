b6f23579b64bfb5ed81ced33b34bd377b9631aa0
held <2259448181@qq.com>
2024-11-26 20:36:22 +0800

    能输出显示

--- src/main/java/com/chentu/mika/controller/ProductController.java
+++ src/main/java/com/chentu/mika/controller/ProductController.java
@@ -1,5 +1,6 @@
 package com.chentu.mika.controller;
 
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.chentu.mika.model.entity.Cat;
 import com.chentu.mika.model.entity.Product;
 import com.chentu.mika.model.form.CatForm;
@@ -8,31 +9,74 @@ import com.chentu.mika.service.CatService;
 import com.chentu.mika.service.ProductService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @description:处理与产品相关的API请求
  */
 @RestController
 public class ProductController {
+
+	@Value("${prop.upload-folder}")
+	private String UPLOAD_FOLDER;
 	
 	@Autowired
 	ProductService productService;
 	
 	@PostMapping("/prdts")
-	public Result add(@RequestBody Product product) {
-		Assert.notNull(product, "参数不能为空");
-		Assert.notNull(product.getProductName(), "商品名称不能为空");
-		Assert.notNull(product.getPrice(), "商品价格不能为空");
-		productService.save(product);
+	@ResponseBody
+	public Result add(@RequestParam("productName") String name,
+					  @RequestParam("productImage") MultipartFile image,
+					  @RequestParam("productBrief") String brief,
+					  @RequestParam("price") Double price)throws FileNotFoundException {
+		Assert.notNull(name, "商品名称不能为空");
+		Assert.notNull(price, "商品价格不能为空");
+
+		Product product=new Product();
+
+		product.setProductImage(""+image.getOriginalFilename().hashCode()+".jpg");
+		product.setProductName(name);
+		product.setPrice(price);
+
+		try {
+			BufferedOutputStream out = new BufferedOutputStream(
+					new FileOutputStream(new File(
+							UPLOAD_FOLDER+image.getOriginalFilename().hashCode()+".jpg")));
+			out.write(image.getBytes());
+			out.flush();
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return Result.fail("上传失败");
+		} catch (IOException e) {
+			e.printStackTrace();
+			return Result.fail("上传失败");
+		}
+
 		return Result.success(null);
 	}
 	
 	@DeleteMapping("/prdts/{productIDs}")
 	public Result delete(@PathVariable("productIDs") String productIDs) {
+		String[] split = productIDs.split(",");
+		List<Integer> list = Arrays.stream(split).map(Integer::parseInt).toList();
+		for (Integer i : list) {
+			File file = new File(UPLOAD_FOLDER
+					+productService
+					.getOne(Wrappers.<Product>lambdaQuery()
+							.eq(Product::getProductID, i)).getProductImage());
+			// 路径为文件且不为空则进行删除
+			if (file.isFile() && file.exists()) {
+				file.delete();
+			}
+		}
 		productService.removeByIds(Arrays.asList(productIDs.split(",")));
 		return Result.success(null);
 	}
@@ -44,8 +88,41 @@ public class ProductController {
 	}
 	
 	@PutMapping("/prdts")
-	public Result update(@RequestBody Product product) {
-		productService.updateById(product);
+	@ResponseBody
+	public Result update(@RequestParam("productImage") MultipartFile image,
+						 @RequestParam("productName") String name,
+						 @RequestParam("price") Double price,
+						 @RequestParam("productBrief") String brief,
+						 @RequestParam("productID") Integer ID) throws FileNotFoundException {
+		File file = new File(UPLOAD_FOLDER
+				+productService
+				.getOne(Wrappers.<Product>lambdaQuery()
+						.eq(Product::getProductID, ID)).getProductImage());
+		// 路径为文件且不为空则进行删除
+		if (file.isFile() && file.exists()) {
+			file.delete();
+		}
+		try {
+			BufferedOutputStream out = new BufferedOutputStream(
+					new FileOutputStream(new File(
+							UPLOAD_FOLDER+image.getOriginalFilename().hashCode()+".jpg")));
+			out.write(image.getBytes());
+			out.flush();
+			out.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			return Result.fail("上传失败");
+		} catch (IOException e) {
+			e.printStackTrace();
+			return Result.fail("上传失败");
+		}
+		productService.update(Wrappers.<Product>lambdaUpdate()
+				.eq(Product::getProductID, ID)
+				.set(name!=null,Product::getProductName, name)
+				.set(image!=null,Product::getProductImage, ""+image.getOriginalFilename().hashCode()+".jpg")
+				.set(brief!=null,Product::getProductBrief, brief)
+				.set(price!=null,Product::getPrice, price)
+		);
 		return Result.success(null);
 	}
 	
